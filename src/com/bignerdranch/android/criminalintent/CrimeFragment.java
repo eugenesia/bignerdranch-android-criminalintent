71a9e2071294d0fd0ab1bd1f2452acd66fdcbb69
Eugene Sia <eugene@eugenesia.co.uk>
2015-04-03 18:40:49 +0100

    Wire up EditText widget lst 7.9

--- src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -2,12 +2,16 @@ package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
+    private EditText mTitleField;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,31 @@ public class CrimeFragment extends Fragment {
         // Set attachToRoot = false as we will add the inflated view to its
         // parent in code.
         View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+        
+        // View v contains an EditText, defined inside R.layout.fragment_crime.
+        mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        
+        // Define a class here to watch for various changes of text.
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            
+            public void onTextChanged(
+                    CharSequence c, int start, int before, int count) {
+                // Store the edited title string.
+                mCrime.setTitle(c.toString());
+            }
+            
+            public void beforeTextChanged(
+                    CharSequence c, int start, int count, int after) {
+                // This space intentionally left blank.
+            }
+            
+            public void afterTextChanged(Editable c) {
+                // This space intentionally left blank.
+            }
+        });
+        
+        // Return inflated fragment view with its listeners back to the
+        // Activity.
         return v;
     }
-
 }
