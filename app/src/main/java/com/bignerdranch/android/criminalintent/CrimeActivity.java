2c806d61ae23970912393dbb921b4e3b3da2bd71
Eugene Sia <eugene@eugenesia.co.uk>
2015-10-05 18:23:25 +0100

    Get FragmentManager lst 7.14 (now in 2ed book)

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -2,14 +2,17 @@ package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.MenuItem;
 
 
 public class CrimeActivity extends FragmentActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_crime);
+
+    FragmentManager fm = getSupportFragmentManager();
+  }
 }
