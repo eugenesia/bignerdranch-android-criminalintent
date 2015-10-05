9af2efb2ade6d253f0ccc203a3b1de944822fc2d
Eugene Sia <eugene@eugenesia.co.uk>
2015-10-05 18:32:05 +0100

    Add a CrimeFragment lst 7.15

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.view.MenuItem;
@@ -14,5 +15,13 @@ public class CrimeActivity extends FragmentActivity {
     setContentView(R.layout.activity_crime);
 
     FragmentManager fm = getSupportFragmentManager();
+    Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+    if (fragment == null) {
+      fragment = new CrimeFragment();
+      fm.beginTransaction()
+        .add(R.id.fragment_container, fragment)
+        .commit();
+    }
   }
 }
