8876b0581ddbe748a74d4564f5c1ebe8a0427238
Eugene Sia <eugene@eugenesia.co.uk>
2015-10-08 22:58:54 +0100

    Create an abstract Activity lst 9.7

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Eugene on 08/10/15.
+ * Abstract reusable Activity that hosts a single Fragment.
+ */
+public abstract class SingleFragmentActivity extends FragmentActivity {
+}
