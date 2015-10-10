30f4f89a30a549f2cd2c6227e3b7501e7af0825c
Eugene Sia <eugene@eugenesia.co.uk>
2015-10-11 00:21:16 +0100

    Set up the view for CrimeListFragment lst 9.16

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -1,9 +1,32 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
- * Created by Eugene on 10/10/15.
+ * Fragment to show a list of crimes.
  */
 public class CrimeListFragment extends Fragment {
+
+  private RecyclerView mCrimeRecyclerView;
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    Bundle savedInstanceState) {
+
+    // Get the RecyclerView showing a list of crimes.
+    View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+    // ID of the container.
+    mCrimeRecyclerView = (RecyclerView) view
+      .findViewById(R.id.crime_recycler_view);
+    mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+    return view;
+  }
 }
