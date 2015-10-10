742095af66624eaead5f85acc7fd25c0aa861076
Eugene Sia <eugene@eugenesia.co.uk>
2015-10-10 05:35:17 +0100

    Implement CrimeListFragment lst 9.11

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Eugene on 10/10/15.
+ */
+public class CrimeListFragment extends Fragment {
+}
