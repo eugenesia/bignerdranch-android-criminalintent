d5ec36eb32ac7dea2e7d6f87d36da1b15b5c1dfc
Eugene Sia <eugene@eugenesia.co.uk>
2015-11-01 00:02:37 +0000

    Create newIntent lst 11.3

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -8,6 +10,7 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Activity to host a Fragment containing a ViewPager, so user can swipe through
@@ -15,14 +18,31 @@ import java.util.List;
  */
 public class CrimePagerActivity extends FragmentActivity {
 
+  // Key for Crime ID in Intent.
+  private static final String EXTRA_CRIME_ID =
+    "com.bignerdranch.android.criminalintent.crime_id";
+
   // View that allows swiping from crime to crime.
   private ViewPager mViewPager;
   private List<Crime> mCrimes;
+
+
+  // Create new Intent to start this Activity, storing the Crime ID.
+  public static Intent newIntent(Context packageContext, UUID crimeId) {
+    Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+    intent.putExtra(EXTRA_CRIME_ID, crimeId);
+    return intent;
+  }
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_crime_pager);
+
+    // Get ID of Crime to show from intent.
+    UUID crimeId = (UUID) getIntent()
+      .getSerializableExtra(EXTRA_CRIME_ID);
 
     mViewPager = (ViewPager) findViewById(R.id.activity_crime_pager_view_pager);
 
