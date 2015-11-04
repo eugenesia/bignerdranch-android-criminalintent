2345fd7b8ffe1138e6001860460659db16e4b089
Eugene Sia <eugene@eugenesia.co.uk>
2015-11-04 00:21:27 +0000

    Add AlertDialog comments, annotate @NonNull

--- app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.criminalintent;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 
@@ -11,9 +12,14 @@ import android.support.v7.app.AlertDialog;
 public class DatePickerFragment extends DialogFragment {
 
   @Override
+  @NonNull
   public Dialog onCreateDialog(Bundle savedInstanceState) {
     return new AlertDialog.Builder(getActivity())
+
+      // Set title of the dialog.
       .setTitle(R.string.date_picker_title)
+
+      // Set OK button but return null listener for now.
       .setPositiveButton(android.R.string.ok, null)
       .create();
   }
