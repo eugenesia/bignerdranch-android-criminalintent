d6f9742ff3824a5ad89deaa86197f9b3f864b030
Eugene Sia <eugene@eugenesia.co.uk>
2015-11-08 00:44:53 +0000

    Add TimePickerFragment class

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/TimePickerFragment.java
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Allow user to select time of day using TimePicker widget.
+ */
+public class TimePickerFragment extends DialogFragment {
+}
