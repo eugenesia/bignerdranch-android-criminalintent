94e9ef027b2060d14d74c92fe9c7bf2f9a62f826
Eugene Sia <eugene@eugenesia.co.uk>
2015-11-09 09:13:22 +0000

    Set TimePickerFragment view

--- app/src/main/java/com/bignerdranch/android/criminalintent/TimePickerFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/TimePickerFragment.java
@@ -1,9 +1,42 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TimePicker;
+
 
 /**
  * Allow user to select time of day using TimePicker widget.
  */
 public class TimePickerFragment extends DialogFragment {
+
+  private TimePicker mTimePicker;
+
+  @Override
+  @NonNull
+  public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+    // The DatePicker View.
+    View v = LayoutInflater.from(getActivity())
+      .inflate(R.layout.dialog_time, null);
+
+    mTimePicker = (TimePicker) v.findViewById(R.id.dialog_time_time_picker);
+
+    return new AlertDialog.Builder(getActivity())
+
+      // Set the AlertDialog to show the inflated layout.
+      .setView(v)
+
+        // Set title of the dialog.
+      .setTitle(R.string.time_picker_title)
+
+        // OK button.
+      .setPositiveButton(android.R.string.ok, null)
+      .create();
+  }
 }
