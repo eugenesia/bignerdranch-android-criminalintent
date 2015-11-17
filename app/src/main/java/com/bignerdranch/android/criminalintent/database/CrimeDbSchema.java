8e5b0adcb08f46ec93877ff614b65b3289127506
Eugene Sia <eugene@eugenesia.co.uk>
2015-11-17 23:41:40 +0000

    Define table columns lst 14.2

--- app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDbSchema.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDbSchema.java
@@ -7,6 +7,21 @@ public class CrimeDbSchema {
 
   // Describe the Crime table in the db.
   public static final class CrimeTable {
+
+    // Name of the table.
     public static final String NAME = "crimes";
+
+    // Columns in the table.
+    //
+    // With that, you will be able to refer to the column named "title" in a
+    // Java-safe way: CrimeTable.Cols.TITLE. That makes it much safer to change
+    // your program if you ever need to change the name of that column or add
+    // additional data to the table.
+    public static final class Cols {
+      public static final String UUID = "uuid";
+      public static final String TITLE = "title";
+      public static final String DATE = "date";
+      public static final String SOLVED = "solved";
+    }
   }
 }
