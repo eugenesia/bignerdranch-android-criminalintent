d04fc7d6dcab035d06aa0b226c37449ca197c7ad
Eugene Sia <eugene@eugenesia.co.uk>
2015-11-17 23:38:51 +0000

    Define CrimeTable lst 14.1

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.criminalintent.database;
+
+/**
+ * Store SQLite database schema.
+ */
+public class CrimeDbSchema {
+
+  // Describe the Crime table in the db.
+  public static final class CrimeTable {
+    public static final String NAME = "crimes";
+  }
+}
