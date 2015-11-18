73be4b69d5cf042131424e576b9e709921aaa1f2
Eugene Sia <eugene@eugenesia.co.uk>
2015-11-18 00:27:52 +0000

    Open a SQLiteDatabase lst 14.4

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -1,6 +1,9 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.bignerdranch.android.criminalintent.database.CrimeBaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,9 +21,20 @@ public class CrimeLab {
   // List of crimes.
   private List<Crime> mCrimes;
 
+  // To make use of it later in Chapter 16.
+  private Context mContext;
+
+  // Database to store crimes.
+  private SQLiteDatabase mDatabase;
+
 
   // Private constructor, only allow access to singleton instance.
   private CrimeLab(Context context) {
+
+    // Open SQLiteDatabase for crimes.
+    mContext = context.getApplicationContext();
+    mDatabase = new CrimeBaseHelper(mContext)
+      .getWritableDatabase();
 
     // The mCrimes instantiation line uses diamond notation, <>, which was introduced in Java 7.
     // This shorthand notation tells the compiler to infer the type of items the List will contain
