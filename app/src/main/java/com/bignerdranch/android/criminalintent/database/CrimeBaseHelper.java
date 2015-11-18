aacc9c05ac652d2f7c0c3846a7567278ac2cbad1
Eugene Sia <eugene@eugenesia.co.uk>
2015-11-18 00:39:30 +0000

    Write first part of onCreate() lst 14.5

--- app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeBaseHelper.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeBaseHelper.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.bignerdranch.android.criminalintent.database.CrimeDbSchema.CrimeTable;
+
 /**
  * Helper functions for handling database.
  *
@@ -27,14 +29,22 @@ public class CrimeBaseHelper extends SQLiteOpenHelper {
   private static final int VERSION = 1;
   private static final String DATABASE_NAME = "crimeBase.db";
 
+
   public CrimeBaseHelper(Context context) {
     super(context, DATABASE_NAME, null, VERSION);
   }
 
+
+  // Called on initial db creation.
   @Override
   public void onCreate(SQLiteDatabase db) {
+    // Execute SQL to create crimes table.
+    db.execSQL("create table " + CrimeTable.NAME);
   }
 
+
+  // Called to upgrade database when db version is less than this class's
+  // version.
   @Override
   public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
   }
