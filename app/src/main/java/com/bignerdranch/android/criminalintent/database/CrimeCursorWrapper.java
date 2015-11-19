86dd55e372e4d6f2bcebe0f384a6ccb4ea4adce3
Eugene Sia <eugene@eugenesia.co.uk>
2015-11-19 01:58:19 +0000

    Add getCrime method lst 14.14

--- app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeCursorWrapper.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeCursorWrapper.java
@@ -1,6 +1,10 @@
 package com.bignerdranch.android.criminalintent.database;
 
 import android.database.Cursor;
+import android.database.CursorWrapper;
+
+import com.bignerdranch.android.criminalintent.Crime;
+import com.bignerdranch.android.criminalintent.database.CrimeDbSchema.CrimeTable;
 
 /**
  * A Cursor leaves a lot to be desired as a way to look at a table. All it does
@@ -10,9 +14,21 @@ import android.database.Cursor;
  * A CursorWrapper lets you wrap a Cursor you received from another place and
  * add new methods on top of it.
  */
-public class CrimeCursorWrapper {
+public class CrimeCursorWrapper extends CursorWrapper {
 
   public CrimeCursorWrapper(Cursor cursor) {
     super(cursor);
+  }
+
+
+  // Read crime from database and format fields.
+  public Crime getCrime() {
+
+    String uuidString = getString(getColumnIndex(CrimeTable.Cols.UUID));
+    String title = getString(getColumnIndex(CrimeTable.Cols.TITLE));
+    long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
+    int isSolved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
+
+    return null;
   }
 }
