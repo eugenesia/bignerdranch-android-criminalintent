09d92bb13112b5a772a1d015705618c881d0a528
Eugene Sia <eugene@eugenesia.co.uk>
2015-11-28 07:00:25 +0000

    Write conservative scale method lst 16.8

--- app/src/main/java/com/bignerdranch/android/criminalintent/PictureUtils.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/PictureUtils.java
@@ -1,12 +1,23 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
 
 /**
  * Image manipulation.
  */
 public class PictureUtils {
+
+  public static Bitmap getScaledBitmap(String path, Activity activity) {
+    Point size = new Point();
+    activity.getWindowManager().getDefaultDisplay()
+      .getSize(size);
+
+    return getScaledBitmap(path, size.x, size.y);
+  }
+
   public static Bitmap getScaledBitmap(String path, int destWidth,
     int destHeight) {
 
